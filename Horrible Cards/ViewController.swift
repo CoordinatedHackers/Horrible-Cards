f231aca5b037b0aac5a79d8b5325732095d33542
Sidney San Martín <sidney@s4y.us>
2014-06-11 00:41:20 -0400

    Working table view

--- Horrible Cards/ViewController.swift
+++ Horrible Cards/ViewController.swift
@@ -8,7 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HandDataSource: NSObject, UITableViewDataSource {
+    func tableView(tableView: UITableView!,
+        cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+            var cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+            cell.text = "Some Horrible Thing"
+            return cell
+    }
+    
+    func tableView(tableView: UITableView!,
+        numberOfRowsInSection section: Int) -> Int {
+            return 5;
+    }
+    
+}
+
+class ViewController: UIViewController, UITableViewDataSource {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +34,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView!,
+        cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
+            let identifier = "card"
+            var cell = tableView.dequeueReusableCellWithIdentifier(identifier) as? UITableViewCell
+            if cell == nil {
+                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: identifier)
+            }
+            cell!.text = "Some Horrible Thing"
+            return cell
+    }
+    
+    func tableView(tableView: UITableView!,
+        numberOfRowsInSection section: Int) -> Int {
+            return 5;
+    }
+
 
 
 }
-
